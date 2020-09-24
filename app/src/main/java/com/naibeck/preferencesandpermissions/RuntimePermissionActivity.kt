1c77fa6ed0fbc74da59e8105338487a3a5242f3e
Kevin Gomez <kg-diseno@outlook.es>
2020-09-23 21:55:05 -0300

    Step 07: Add rationale when user denied access to permission

--- app/src/main/java/com/naibeck/preferencesandpermissions/RuntimePermissionActivity.kt
+++ app/src/main/java/com/naibeck/preferencesandpermissions/RuntimePermissionActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.naibeck.preferencesandpermissions.databinding.ActivityRuntimePermissionBinding
@@ -37,8 +38,21 @@ class RuntimePermissionActivity : AppCompatActivity() {
         if (isFineLocationGranted(grantResults)) {
             Toast.makeText(this, "Location permission was granted", Toast.LENGTH_SHORT).show()
         } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_FINE_LOCATION)) {
+                showLocationRationale()
+            }
             Toast.makeText(this, "Location permission was not granted", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun showLocationRationale() {
+        AlertDialog.Builder(this)
+            .setTitle("Wanna know your position?")
+            .setMessage("This application requires user's location to show users position on Map")
+            .setPositiveButton("Ok") { _, _ -> requestRuntimePermission()}
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show()
     }
 
     private fun isFineLocationGranted(grantResults: IntArray) =
